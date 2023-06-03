5db5937278a82e6bf28ce584240223f05b7a3f47
Atom <113407482+Jintong-Liu@users.noreply.github.com>
2023-06-03 10:12:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ install_target="/usr/local/bin/dp"
 post_install() {
   if [ $? -eq 0 ]
   then
-    echo "Install successfully"
+    echo "Install successfully, cli path: $install_target"
     exit 0
   else
     echo "Install failed"
@@ -19,13 +19,15 @@ install() {
   select opt in aarch64-apple-darwin x86_64-apple-darwin; do
     case $opt in
       aarch64-apple-darwin)
-        wget -O $install_target "$download_url/dp_aarch64-apple-darwin"
-        chmod +x $install_target
+        echo "Installing for arm64, sudo required..."
+        sudo wget -O $install_target "$download_url/dp_aarch64-apple-darwin"
+        sudo chmod +x $install_target
         post_install
         ;;
       x86_64-apple-darwin)
-        wget -O $install_target "$download_url/dp_x86_64-apple-darwin"
-        chmod +x $install_target
+        echo "Installing for x86_64, sudo required..."
+        sudo wget -O $install_target "$download_url/dp_x86_64-apple-darwin"
+        sudo chmod +x $install_target
         echo "Install successfully"
         post_install
         ;;
